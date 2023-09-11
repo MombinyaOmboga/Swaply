87da3d9dbafb3b993ade5a529dd5f9f013773665
Mombinya <bigshemo@gmail.com>
2023-09-11 22:13:29 +0300

    url mapping

--- Swaply/Swaply/urls.py
+++ Swaply/Swaply/urls.py
@@ -1,13 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-
-from core.views import contact, index
+from django.urls import include, path
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('items/', include('Items.urls')),
-    path('contact/', contact, name='contact'),
+    path('', include('core.urls')),
+    path('item/', include('Items.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
